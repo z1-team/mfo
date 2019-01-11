b6f0249a319f668fabeb85d5c3b06d9bc6747ba6
Vladimir Shalaev <vladimir-shalaev22@yandex.ru>
2019-01-11 18:34:03 +0700

    Обновляет ResultContainer - Рефакторинг фронтэнда

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,17 +1,25 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
 
 import Card from './card'
 
-const Results = ({tail, isLoggedIn, partners, onOrder, onEdit, onMore}) => (
-  <div class="list">
-    {isLoggedIn && <button class="add-card" onClick={this.handleAdd}>Добавить партнера</button>}
-    {partners.map((partner) => (
-      <Card key={partner.id} item={partner}
-        tail={tail} onOrder={onOrder}
-        edit={isLoggedIn} onEdit={onEdit}
-        dataID={partner.id} onMore={onMore}/>
-    ))}
-  </div>
-)
+class Results extends Component {
+  componentDidMount() {
+    this.props.onEnter()
+  }
+
+  render({tail, isLoggedIn, partners, onOrder, onEdit, onMore}) {
+    return (
+      <div class="list">
+        {isLoggedIn && <button class="add-card" onClick={this.handleAdd}>Добавить партнера</button>}
+        {partners.map((partner) => (
+          <Card key={partner.id} item={partner}
+            tail={tail} onOrder={onOrder}
+            edit={isLoggedIn} onEdit={onEdit}
+            dataID={partner.id} onMore={onMore}/>
+        ))}
+      </div>
+    )
+  }
+}
 
 export default Results
