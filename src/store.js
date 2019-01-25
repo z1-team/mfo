7b1e3cb1443d3cefd682fb919c4132ebf2a742e1
Vasilchikov Aleksey <alekseyvi1303@gmail.com>
2019-01-25 14:00:58 +0300

    up: отключение redux-devtool

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import appReducer from './reducers'
 
-const store = createStore(appReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(appReducer, applyMiddleware(thunk))
 
 export default store
