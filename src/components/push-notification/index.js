0e00cb3f2379c5c0edc1f366a59893af158f48ed
Aleksey <alekseyvi1303@gmail.com>
2019-04-18 13:29:45 +0300

    Доработки по занять-денег.онлайн

--- src/components/push-notification/index.js
+++ src/components/push-notification/index.js
@@ -7,7 +7,7 @@ import Button from '../utility/Button'
 class Push extends Component {
   render({isOpen, onAccept, onDecline}) {
     return (
-      <div class={`${style.push} ${isOpen ? style.active : ''}`}>
+      <div class={`${style.normal} ${isOpen ? style.active : ''}`}>
         <button class={style.closeButton} onClick={onDecline}></button>
         <p>Раз в 2-3 недели мы добавляем проверенные займы. Хотите получать их спецпредложения для первых клиентов?</p>
         <ul>
