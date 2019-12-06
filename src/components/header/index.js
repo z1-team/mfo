52828337d7a692b67e354e1255cec7c7562ce13c
Aleksey <alekseyvi1303@gmail.com>
2019-12-06 15:23:40 +0300

    Update: правки дизайна

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,13 +6,13 @@ import Icon from 'common/components/Icon'
 import style from './style.scss'
 
 const Header = ({place, showBot}) => <HeaderCommon place={place} showBot={showBot}>
-	<div class={style.contacts}>
-		<p class={style.contactsText}><Icon icon="marker" />{place}</p>
-	</div>
 	<ul class={style.links}>
 		<li><Link name="mfo" activeClassName={style.active} href="/">Микрозаймы</Link></li>
 		<li><Link name="cards" activeClassName={style.active} href="/cards">Кредитные карты</Link></li>
 	</ul>
+	<div class={style.contacts}>
+		<p class={style.contactsText}><Icon icon="marker" />{place}</p>
+	</div>
 </HeaderCommon>
 
 export default Header
--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -19,12 +19,12 @@ const Main = ({partners}) => (
 	<div class={style.wrMain}>
 		<div class="container">
 			<div class={style.main}>
-				<FilterContainer partners={partners} />
 				<div class={style.results} id="results">
 					<h2>Рейтинг {getTitle(partners)} <em>Рунета 2019 года</em></h2>
 					<SortContainer partners={partners} />
 					<ResultContainer partners={partners} />
 				</div>
+				<FilterContainer partners={partners} />
 			</div>
 		</div>
 	</div>
