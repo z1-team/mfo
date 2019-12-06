d0048c5b8ee162160a0d374cb93f07e07bd02ccf
Vladimir Shalaev <vladimir-shalaev22@yandex.ru>
2019-12-06 14:47:18 +0700

    Изменяет пути для API-сервера

--- src/api/config.js
+++ src/api/config.js
@@ -4,7 +4,7 @@ const fullURL = url => baseURL + url
 function getBaseURL() {
   if (typeof window !== 'undefined') {
     return window.location.hostname === 'localhost' ?
-      'https://xn----7sbkdfaj3cd0c0g8a.xn--80asehdb/api/' : '/api/'
+      'https://api.xn----7sbkdfaj3cd0c0g8a.xn--80asehdb/' : '/api/'
   } else {
     return 'http://localhost:3000/'
   }
